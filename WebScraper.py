240892f939f41d964d3c5e902a5021771a1b9ade
CPinkston <cpinksto@gmail.com>
2014-08-06 16:41:10 -0700

    Added the open connection to return from URL method

--- WebScraper.py
+++ WebScraper.py
@@ -6,9 +6,8 @@ def siteData(site):
 	url = urllib2.urlopen(site)
 	data = url.info()
 	html = url.read()
-	return data, html
+	return url, data, html
 
 if __name__ == "__main__":
-	info, content = siteData('http://sports.espn.go.com/nhl/boxscore?gameId=400484244')
-	print info
-	print len(content)
+	site, info, content = siteData('http://sports.espn.go.com/nhl/boxscore?gameId=400484244')
+	print site
